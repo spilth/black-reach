50c68dc9564f45f5a28d2dc0ae2cc41847d6db7d
Brian Kelly <brian@spilth.org>
2025-04-11 07:17:59 -0500

    Use shorter expansion directories and back into unit image path (#2)

--- src/components/UnitTokenImage.tsx
+++ src/components/UnitTokenImage.tsx
@@ -1,8 +1,6 @@
 import { type Unit } from "../data/units.tsx";
 import React from "react";
 
-const unitImageSource = (unit: Unit) => `/units/${unit.release.toLowerCase().replace(/ /g, "-")}/${unit.image}`;
-
 export const UnitTokenImage = ({ unit }: { unit: Unit }) => {
-  return <img src={unitImageSource(unit)} alt={unit.name} className="img-fluid p-2" />;
+  return <img src={`/units/${unit.image}`} alt={unit.name} className="img-fluid p-2" />;
 };
