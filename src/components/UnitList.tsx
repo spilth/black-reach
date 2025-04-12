33058cf4bb262b7eefa80e1a21a064f218d9c661
Brian Kelly <brian@spilth.org>
2025-04-11 21:16:14 -0500

    Adds Unit stats and images from Bad Moon Reinforcements

--- src/components/UnitList.tsx
+++ src/components/UnitList.tsx
@@ -1,26 +1,31 @@
 import React from "react";
-import { Faction, type Unit, units } from "../data/units.tsx";
+import { Faction, Release, type Unit, units } from "../data/units.tsx";
 import { Accordion } from "react-bootstrap";
 import UnitDetails from "./UnitDetails.tsx";
 import { FactionAccordionItem } from "./FactionAccordionItem.tsx";
 import { UnitTokenImage } from "./UnitTokenImage.tsx";
 
 const UnitList = () => {
-  const ultramarines = units.filter(unit => unit.faction == Faction.Ultramarines);
-  const orks = units.filter(unit => unit.faction == Faction.Orks);
+  const cbUltramarines = units.filter(unit => unit.release == Release.CB && unit.faction == Faction.Ultramarines);
+  const cbOrks = units.filter(unit => unit.release == Release.CB && unit.faction == Faction.Orks);
+  const bmOrks = units.filter(unit => unit.release == Release.BM && unit.faction == Faction.Orks);
 
-  const [selectedUnit, setSelectedUnit] = React.useState<Unit>(ultramarines[0]);
+  const [selectedUnit, setSelectedUnit] = React.useState<Unit>(cbUltramarines[0]);
 
   return <div className="pe-2">
     <div className="row g-0 vh-100">
       <div className="col-3 col-lg-2 border-end">
         <Accordion defaultActiveKey="Ultramarines" flush>
-          <FactionAccordionItem name="Ultramarines"
-                                units={ultramarines}
+          <FactionAccordionItem name="Core Box - Ultramarines"
+                                units={cbUltramarines}
                                 onClick={setSelectedUnit}
                                 isActive={(unit: Unit) => unit === selectedUnit} />
-          <FactionAccordionItem name="Orks"
-                                units={orks}
+          <FactionAccordionItem name="Core Box - Orks"
+                                units={cbOrks}
+                                onClick={setSelectedUnit}
+                                isActive={(unit: Unit) => unit === selectedUnit} />
+          <FactionAccordionItem name="Bad Moon - Orks"
+                                units={bmOrks}
                                 onClick={setSelectedUnit}
                                 isActive={(unit: Unit) => unit === selectedUnit} />
 
--- src/data/units.tsx
+++ src/data/units.tsx
@@ -34,6 +34,7 @@ export enum Size {
   Vehicle = "Vehicle",            // 472x768
   LargeVehicle = "Large Vehicle", // 906x906
   LongVehicle = "Long Vehicle",   // 466x929
+  SmallTitan = "Small Titan",     // 1400x1400
 }
 
 export interface Weapon {
@@ -95,14 +96,14 @@ export const units: Unit[] = [
       {
         infantry: 2,
         lightVehicles: 3,
-        heavyVehicle: 2,
+        heavyVehicles: 2,
         abilities: [Ability.IndirectFire]
       }
     ],
     secondaryWeapon: {
       infantry: 3,
       lightVehicles: 3,
-      heavyVehicle: 0,
+      heavyVehicles: 0,
       abilities: [Ability.DoubleFire, Ability.FireOnTheMove, Ability.MachineGunner, Ability.SuppressiveFire]
     }
   },
@@ -183,14 +184,14 @@ export const units: Unit[] = [
       {
         infantry: 2,
         lightVehicles: 5,
-        heavyVehicle: 4,
+        heavyVehicles: 4,
         abilities: [Ability.TwinLinked, Ability.Piercing, Ability.FireOnTheMove]
       }
     ],
     secondaryWeapon: {
       infantry: 2,
       lightVehicles: 1,
-      heavyVehicle: 0,
+      heavyVehicles: 0,
       abilities: [Ability.Assault, Ability.FireOnTheMove, Ability.Demolishing]
     }
   },
@@ -241,14 +242,14 @@ export const units: Unit[] = [
       {
         infantry: 1,
         lightVehicles: 4,
-        heavyVehicle: 4,
+        heavyVehicles: 4,
         abilities: [Ability.Demolishing, Ability.LimitedRange]
       }
     ],
     secondaryWeapon: {
       infantry: 3,
       lightVehicles: 3,
-      heavyVehicle: 0,
+      heavyVehicles: 0,
       abilities: [Ability.Assault, Ability.Charge, Ability.FireOnTheMove]
     }
   },
@@ -345,14 +346,14 @@ export const units: Unit[] = [
       {
         infantry: null,
         lightVehicles: null,
-        heavyVehicle: null,
+        heavyVehicles: null,
         abilities: [Ability.LimitedFiringArc, Ability.Flamethrower]
       }
     ],
     secondaryWeapon: {
       infantry: 5,
       lightVehicles: 3,
-      heavyVehicle: 2,
+      heavyVehicles: 2,
       abilities: [Ability.TwinLinked, Ability.MachineGunner, Ability.SuppressiveFire, Ability.FireOnTheMove]
     }
   },
@@ -464,14 +465,14 @@ export const units: Unit[] = [
       {
         infantry: 4,
         lightVehicles: 4,
-        heavyVehicle: 2,
+        heavyVehicles: 2,
         abilities: [Ability.MachineGunner, Ability.SuppressiveFire, Ability.FireOnTheMove]
       }
     ],
     secondaryWeapon: {
       infantry: 2,
       lightVehicles: 5,
-      heavyVehicle: 4,
+      heavyVehicles: 4,
       abilities: [Ability.Piercing, Ability.FireOnTheMove]
     }
   },
@@ -493,14 +494,14 @@ export const units: Unit[] = [
       {
         infantry: 4,
         lightVehicles: 3,
-        heavyVehicle: 1,
+        heavyVehicles: 1,
         abilities: [Ability.TwinLinked, Ability.MachineGunner, Ability.SuppressiveFire, Ability.FireOnTheMove]
       }
     ],
     secondaryWeapon: {
       infantry: 2,
       lightVehicles: 1,
-      heavyVehicle: null,
+      heavyVehicles: null,
       abilities: [Ability.SuppressiveFire, Ability.FireOnTheMove]
     }
   },
@@ -782,6 +783,324 @@ export const units: Unit[] = [
     role: Role.Regular,
     size: Size.Vehicle,
     image: "cb/warbuggy.jpg",
+    movement: 6,
+    defence: 5,
+    infantry: 3,
+    lightVehicles: 3,
+    heavyVehicles: null,
+    abilities: [Ability.SuppressiveFire, Ability.MachineGunner, Ability.FireOnTheMove]
+  },
+  {
+    release: Release.BM,
+    name: "Big Shoota",
+    faction: Faction.Orks,
+    type: Type.Infantry,
+    role: Role.HeavyWeapons,
+    size: Size.Squad,
+    image: "bm/big-shoota.jpg",
+    movement: 3,
+    defence: 4,
+    infantry: 3,
+    lightVehicles: 3,
+    heavyVehicles: null,
+    abilities: [Ability.Assault, Ability.SuppressiveFire]
+  },
+  {
+    release: Release.BM,
+    name: "Bomb Squigs",
+    faction: Faction.Orks,
+    type: Type.Infantry,
+    role: Role.Regular,
+    size: Size.Individual,
+    image: "bm/bomb-squigs.jpg",
+    movement: 4,
+    defence: 6,
+    infantry: 3,
+    lightVehicles: 2,
+    heavyVehicles: null,
+    abilities: [Ability.Explosion, Ability.Assault, Ability.LimitedRange]
+  },
+  {
+    release: Release.BM,
+    name: "Grot Mega Tank",
+    faction: Faction.Orks,
+    type: Type.HeavyVehicle,
+    role: Role.Regular,
+    size: Size.LongVehicle,
+    image: "bm/grot-mega-tank.jpg",
+    movement: 5,
+    defence: 7,
+    infantry: null,
+    lightVehicles: null,
+    heavyVehicles: null,
+    abilities: [],
+    primaryWeapons: [
+      {
+        infantry: 3,
+        lightVehicles: 3,
+        heavyVehicles: 2,
+        abilities: [Ability.TwinLinked, Ability.LimitedRange]
+      }
+    ],
+    secondaryWeapon: {
+      infantry: 3,
+      lightVehicles: 3,
+      heavyVehicles: null,
+      abilities: [Ability.Flamethrower, Ability.SuppressiveFire]
+    }
+  },
+  {
+    release: Release.BM,
+    name: "Grot Tank (A)",
+    faction: Faction.Orks,
+    type: Type.LightVehicle,
+    role: Role.Regular,
+    size: Size.Squad,
+    image: "bm/grot-tank-a.jpg",
+    movement: 5,
+    defence: 7,
+    infantry: 1,
+    lightVehicles: 0,
+    heavyVehicles: null,
+    abilities: [Ability.Flamethrower]
+  },
+  {
+    release: Release.BM,
+    name: "Grot Tank (B)",
+    faction: Faction.Orks,
+    type: Type.LightVehicle,
+    role: Role.Regular,
+    size: Size.Squad,
+    image: "bm/grot-tank-b.jpg",
+    movement: 5,
+    defence: 7,
+    infantry: 1,
+    lightVehicles: 4,
+    heavyVehicles: 4,
+    abilities: [Ability.Destruction, Ability.FireOnTheMove, Ability.LimitedRange]
+  },
+  {
+    release: Release.BM,
+    name: "Grot Tank (C)",
+    faction: Faction.Orks,
+    type: Type.LightVehicle,
+    role: Role.Regular,
+    size: Size.Squad,
+    image: "bm/grot-tank-c.jpg",
+    movement: 5,
+    defence: 7,
+    infantry: 3,
+    lightVehicles: 3,
+    heavyVehicles: null,
+    abilities: [Ability.SuppressiveFire, Ability.FireOnTheMove]
+  },
+  {
+    release: Release.BM,
+    name: "Killa Kan (A)",
+    faction: Faction.Orks,
+    type: Type.LightVehicle,
+    role: Role.Regular,
+    size: Size.Squad,
+    image: "bm/killa-kan-a.jpg",
+    movement: 3,
+    defence: 8,
+    infantry: 3,
+    lightVehicles: 2,
+    heavyVehicles: 1,
+    abilities: [Ability.LimitedRange, Ability.Assault, Ability.Tearing, Ability.SuppressiveFire, Ability.FireOnTheMove]
+  },
+  {
+    release: Release.BM,
+    name: "Killa Kan (B)",
+    faction: Faction.Orks,
+    type: Type.LightVehicle,
+    role: Role.Regular,
+    size: Size.Squad,
+    image: "bm/killa-kan-b.jpg",
+    movement: 3,
+    defence: 8,
+    infantry: 4,
+    lightVehicles: 4,
+    heavyVehicles: 4,
+    abilities: [Ability.Assault, Ability.Piercing, Ability.FireOnTheMove]
+  },
+  {
+    release: Release.BM,
+    name: "Killa Kan (C)",
+    faction: Faction.Orks,
+    type: Type.LightVehicle,
+    role: Role.Regular,
+    size: Size.Squad,
+    image: "bm/killa-kan-c.jpg",
+    movement: 3,
+    defence: 8,
+    infantry: 3,
+    lightVehicles: 4,
+    heavyVehicles: 4,
+    abilities: [Ability.Assault, Ability.Tearing, Ability.FireOnTheMove, Ability.LimitedRange]
+  },
+  {
+    release: Release.BM,
+    name: "Mega Nob (A)",
+    faction: Faction.Orks,
+    type: Type.Infantry,
+    role: Role.Leader,
+    size: Size.Squad,
+    image: "bm/mega-nob-a.jpg",
+    movement: 3,
+    defence: 6,
+    infantry: 2,
+    lightVehicles: 3,
+    heavyVehicles: 2,
+    abilities: [Ability.Large, Ability.Assault, Ability.Piercing, Ability.FireOnTheMove, Ability.LimitedRange]
+  },
+  {
+    release: Release.BM,
+    name: "Mega Nob (B)",
+    faction: Faction.Orks,
+    type: Type.Infantry,
+    role: Role.Leader,
+    size: Size.Squad,
+    image: "bm/mega-nob-b.jpg",
+    movement: 3,
+    defence: 6,
+    infantry: 2,
+    lightVehicles: 2,
+    heavyVehicles: 0,
+    abilities: [Ability.Large, Ability.Assault, Ability.Tearing, Ability.Flamethrower]
+  },
+  {
+    release: Release.BM,
+    name: "Mega Nob (C)",
+    faction: Faction.Orks,
+    type: Type.Infantry,
+    role: Role.Leader,
+    size: Size.Squad,
+    image: "bm/mega-nob-c.jpg",
+    movement: 3,
+    defence: 6,
+    infantry: 3,
+    lightVehicles: 2,
+    heavyVehicles: 1,
+    abilities: [Ability.Large, Ability.Assault, Ability.Tearing, Ability.Piercing, Ability.FireOnTheMove]
+  },
+  {
+    release: Release.BM,
+    name: "Meka Dread",
+    faction: Faction.Orks,
+    type: Type.HeavyVehicle,
+    role: Role.Regular,
+    size: Size.Mech,
+    image: "bm/meka-dread.jpg",
+    movement: 3,
+    defence: 8,
+    infantry: null,
+    lightVehicles: null,
+    heavyVehicles: null,
+    abilities: [Ability.Mekaniak, Ability.PersonalOrder],
+    primaryWeapons: [
+      {
+        infantry: 3,
+        lightVehicles: 3,
+        heavyVehicles: 3,
+        abilities: [Ability.Piercing, Ability.FrighteningShot, Ability.FireOnTheMove],
+      }
+    ],
+    secondaryWeapon: {
+      infantry: 4,
+      lightVehicles: 3,
+      heavyVehicles: 2,
+      abilities: [Ability.IndirectFire, Ability.Demolishing, Ability.Assault, Ability.Charge]
+    }
+  },
+  {
+    release: Release.BM,
+    name: "Rokkit Launcha",
+    faction: Faction.Orks,
+    type: Type.Infantry,
+    role: Role.HeavyWeapons,
+    size: Size.Squad,
+    image: "bm/rokkit-launcha.jpg",
+    movement: 3,
+    defence: 4,
+    infantry: 1,
+    lightVehicles: 4,
+    heavyVehicles: 4,
+    abilities: [Ability.Assault, Ability.Destruction, Ability.LimitedRange]
+  },
+  {
+    release: Release.BM,
+    name: "Skaablitz",
+    faction: Faction.Orks,
+    type: Type.Infantry,
+    role: Role.Leader,
+    size: Size.Individual,
+    image: "bm/skaablitz.jpg",
+    movement: 4,
+    defence: 6,
+    infantry: 2,
+    lightVehicles: 2,
+    heavyVehicles: 0,
+    abilities: [Ability.Order, Ability.Large, Ability.Assault, Ability.Tearing, Ability.Charge, Ability.Howitzer]
+  },
+  {
+    release: Release.BM,
+    name: "Stompa",
+    faction: Faction.Orks,
+    type: Type.HeavyVehicle,
+    role: Role.Regular,
+    size: Size.SmallTitan,
+    image: "bm/stompa.jpg",
+    movement: 2,
+    defence: 9,
+    infantry: null,
+    lightVehicles: null,
+    heavyVehicles: null,
+    abilities: [Ability.PersonalOrder, Ability.Transport, Ability.Titan],
+    primaryWeapons: [
+      {
+        infantry: 3,
+        lightVehicles: 3,
+        heavyVehicles: 0,
+        abilities: [Ability.IndirectFire, Ability.TwinLinked, Ability.SuppressiveFire, Ability.FireOnTheMove],
+      },
+      {
+        infantry: 4,
+        lightVehicles: 6,
+        heavyVehicles: 5,
+        abilities: [Ability.Charge, Ability.LimitedRange, Ability.Destruction, Ability.Tearing]
+      }
+    ],
+    secondaryWeapon: {
+      infantry: 3,
+      lightVehicles: 4,
+      heavyVehicles: 3,
+      abilities: [Ability.Flamethrower, Ability.SuppressiveFire, Ability.FireOnTheMove, Ability.DoubleFire],
+    }
+  },
+  {
+    release: Release.BM,
+    name: "Trukk",
+    faction: Faction.Orks,
+    type: Type.LightVehicle,
+    role: Role.Regular,
+    size: Size.LongVehicle,
+    image: "bm/trukk.jpg",
+    movement: 6,
+    defence: 8,
+    infantry: 3,
+    lightVehicles: 3,
+    heavyVehicles: null,
+    abilities: [Ability.Transport, Ability.FireOnTheMove, Ability.SuppressiveFire],
+  },
+  {
+    release: Release.BM,
+    name: "Warbuggy",
+    faction: Faction.Orks,
+    type: Type.LightVehicle,
+    role: Role.Regular,
+    size: Size.Vehicle,
+    image: "bm/warbuggy.jpg",
     movement: 6,
     defence: 5,
     infantry: 3,
